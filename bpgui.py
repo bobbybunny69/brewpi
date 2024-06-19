8f911be95e9a717b1ac46350cf947cbeace53652
bobbybunny69 <rob@speckledfrog.com>
2024-06-20 00:01:10 +0100

    Indicators working

--- bpgui.py
+++ bpgui.py
@@ -10,7 +10,7 @@ THEME_COLOR2 = 'white'
 THEME_COLOR3 = 'yellow'
 PRESSED_COLOR = 'blue'
 DISABLED_COLOR ='darkslategray4'
-OFF_COLOR = 'darkslategray4'
+OFF_COLOR = 'grey14'
 
 class BPGui():
     def __init__(self, screen_x, screen_y):
@@ -39,9 +39,13 @@ class BPGui():
         self.rims_reading = Reading(120, 260, 320, 'RIMS Tube')
         self.boil_reading = Reading(470, 260, 320, 'Boil Kettle')
         self.p1_switch= Switch(600, 320, 'On')
+        self.p1_ind = Indicator (700, 306, PRESSED_COLOR, ['assets/pump-1.png', 'assets/pump-2.png', 'assets/pump-3.png'])
         self.p2_switch= Switch(600, 400, 'On')
+        self.p2_ind = Indicator (700, 386, PRESSED_COLOR, ['assets/pump-1.png', 'assets/pump-2.png', 'assets/pump-3.png'])
         self.rims_switch = Switch(140, 180, 'Auto', disabled=True)
+        self.rims_ind = Indicator (240, 166, 'red', ['assets/fire-1.png', 'assets/fire-2.png'])
         self.hlt_switch = Switch(490, 180, 'Auto')
+        self.hlt_ind = Indicator (590, 166, 'red', ['assets/fire-1.png', 'assets/fire-2.png'])
 
     async def update_scrn(self, bp: brewproc.Proc):
         # Read temp probes using a non-blocking task 
@@ -62,11 +66,16 @@ class BPGui():
         self.hlt_up_button.draw(self.screen)
         self.hlt_dn_button.draw(self.screen)
         self.p1_switch.draw(self.screen)
+        self.p1_ind.draw(self.screen, bp.get_pump_state(1))
         self.p2_switch.draw(self.screen)
+        self.p2_ind.draw(self.screen, bp.get_pump_state(2))
         self.rims_switch.draw(self.screen)
+        self.rims_ind.draw(self.screen, bp.get_RIMS_state())
         self.hlt_switch.draw(self.screen)
+        self.hlt_ind.draw(self.screen, bp.get_HLT_state())
+        
         pygame.display.update()
-        await asyncio.sleep(0.223)
+        await asyncio.sleep(0.210)
         self.clock.tick(0)
         logging.debug("Number ms between last 2 ticks: %d", self.clock.get_time())     
         logging.debug("RIMS Auto = {}, HLT Auto = {}, Relays = {}".format(bp.controllers[0].get('allow_heat'), bp.controllers[1].get('allow_heat'), bp.relay_chans_state))
@@ -174,14 +183,14 @@ class Indicator(BPGui):
     def __init__(self, x, y, on_color, icons):
         self.x = x
         self.y = y
-        self.on = False
+        self.state = False
         self.on_color = on_color
         self.i = 0
-        self.img = pygame.image.load(icons[self.i]).convert_alpha()
+        self.img = pygame.image.load(icons[0])  # just to get size
         self.size = self.img.get_rect().width
         self.icons = icons
-    def draw(self, screen: pygame.Surface) :
-        if self.on:
+    def draw(self, screen: pygame.Surface, state=False) :
+        if state:
             self.i += 1
             if self.i==len(self.icons):
                 self.i = 0
@@ -189,9 +198,13 @@ class Indicator(BPGui):
         else:
             self.i = 0
             img_color = OFF_COLOR
-        self.img = pygame.image.load(self.icons[self.i]).convert_alpha()
-        screen.blit(self.img,(self.x, self.y))
-        logging.debug("State: {},  Index: {}, Img color: {}".format(self.on, self.i, img_color))
+        pygame.draw.rect(screen, 'black', (self.x, self.y, self.size, self.size)) # clear
+        image = pygame.image.load(self.icons[self.i]).convert_alpha()
+        color_image = pygame.Surface(image.get_size()).convert_alpha()
+        color_image.fill(img_color)
+        image.blit(color_image, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
+        screen.blit(image,(self.x, self.y))
+        #logging.debug("State: {},  Index: {}, Img color: {}".format(state, self.i, img_color))
 
 class Panel(BPGui):
     def __init__(self, x, y, width, name):
